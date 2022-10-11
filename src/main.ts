1aa723fdab09a4033a4f614e5b5e48441575bfba
Tricia Amheiser <tricia@taarts.net>
2022-10-11 13:02:00 -0400

    named Exports/Imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import './style.css'
 import randomInteger from './randomInteger'
 
-const role = randomInteger(1, 6)
+// modules can export more than one thing
+
+// NAMED exports
+// Import the named export 'diagonalLength'
+// imports the named export "square"
+import { diagonalLength as pythagoreanLength, square } from './util'
+
+const role = randomInteger(1, 6) + 1
 console.log(`you just rolled a ${role}!`)
+
+console.log(pythagoreanLength(4, 3))
+console.log(`the square of 12 is ${square(12)}`)
---
+++ src/util.ts
@@ -0,0 +1,14 @@
+export function squRoot(number: number) {
+  return Math.sqrt(number)
+}
+
+export function square(x: number) {
+  return x * x
+}
+
+export function diagonalLength(x: number, y: number) {
+  return squRoot(square(x) + square(y))
+}
+
+export const PI = 3.14159265
+export const myName = 'Shirley'
